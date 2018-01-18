e1ea6e1fb77e0b93aa56b11d918e865153fc0d83
Harvey Zack <laozhaochaguan@gmail.com>
2018-01-18 16:38:05 +0800

    update

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -4,11 +4,38 @@ const app = getApp();
 
 Page({
   data: {
-
+    storyTitle: "storyTitle", // 命名冲突
+    content: "content",
+    rules: [],
+    wordLimit: 150,
+    catalogue: "catalogue",
+    tags: [],
+    auditMode: 1
   },
-  onLoad() {
 
+  // 数据变化
+  _handleZanFieldChange(e){
+    let name = e.target.dataset.name;
+    let value = e.detail.value;
+    if (name === 'storyTitle' || name === 'content'){
+      this.setData({
+        [name]: value
+      });
+    }
   },
+
+  // 失去焦点
+  _handleZanFieldBlur({ componentId, detail }) {},
+
+  // 获得焦点
+  _handleZanFieldFocus({ componentId, detail }) {},
+
+  // 提交表单
+  formSubmit: function (e) {
+    console.log(e.detail);
+  },
+
+  // 显示
   onShow(){
     let options = util.getOptions();
     if (options.id) {
@@ -17,6 +44,13 @@ Page({
       });
     }
   },
+
+  // 载入
+  onLoad() {
+
+  },
+
+  // 载入就绪
   onReady() {
 
   }
