58a1742ab69ba9dcf788a62fc329ad9905e2e18e
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-20 12:15:47 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,72 +1,58 @@
-const AV = require('../../libs/av-weapp-min.js');
+const { Toast, Tab, extend } = require('../../libs/zanui/index.js');
 const util = require('../../utils/util.js');
-const profile = require('../../api/profile.js');
 const app = getApp();
 
-Page({
-  /**
-   * ================================== 响应数据 ==================================
-   */
-
+Page(extend({}, Toast, Tab, {
   data: {
-    userInfo: null
+    userInfo: null,
+    tab: {
+      selectedId: 'continued',
+      height: 45,
+      list: [{
+        id: 'continued',
+        title: '待续'
+      }, {
+        id: 'popular',
+        title: '热门'
+      }, {
+        id: 'latest',
+        title: '最新'
+      }, {
+        id: 'discover',
+        title: '发现'
+      }]
+    }
   },
 
-  /**
-   * ================================== 事件绑定 ==================================
-   */
-
-  // 事件处理
-  bindViewTap() {
-    console.log(app.globalData)
+  handleZanTabChange(e) {
+    let selectedId = e.selectedId;
+    this.setData({
+      'tab.selectedId': selectedId
+    })
   },
 
-  /**
-   * ================================== 内置事件 ==================================
-   */
-
-  // 页面加载
   onLoad(options) {
     util.pageInit(this, app);
     options.id && profile.openByShare({ id: options.id });
   },
 
-  // 页面展示
   onShow() {
     let options = util.getOptions();
     console.log("onShow")
   },
 
-  // 渲染完成
-  onReady(){
-    console.log("onReady")
-  },
-
-  // 页面隐藏
-  onHide(){
-    console.log("onHide")
-  },
-
-  // 页面卸载 
-  onUnload(){
-    console.log("onUnload")
-  },
-
-  // 下拉动作
-  onPullDownRefresh(){
+  onPullDownRefresh() {
     console.log("onPullDownRefresh")
   },
 
-  // 上拉触底
-  onReachBottom(){
+  onReachBottom() {
     console.log("onReachBottom")
   },
 
-  // 转发
-  onShareAppMessage(){
+  onShareAppMessage() {
     return {
       title: '用你简洁的文字来续写故事吧',
       path: '/pages/index/index?id=' + app.globalData.userInfo.objectId
     }
   }
-})
+}));
