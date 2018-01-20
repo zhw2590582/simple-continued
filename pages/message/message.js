e6a550725a439bde30c703428b23d7f933a85e2c
Harvey Zack <laozhaochaguan@hotmail.com>
2018-01-20 11:46:07 +0800

    fix

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,9 +1,34 @@
-const { Toast, extend } = require('../../libs/zanui/index.js');
+const { Toast, Tab, extend } = require('../../libs/zanui/index.js');
 const story = require('../../api/story.js');
 const util = require('../../utils/util.js');
 const config = require('../../config/index.js');
 const app = getApp();
 
-Page(extend({}, Toast, {
-  
+Page(extend({}, Toast, Tab, {
+  data: {
+    tab: {
+      selectedId: 'round',
+      height: 45,
+      list: [{
+        id: 'round',
+        title: '回合'
+      }, {
+        id: 'comment',
+        title: '评论'
+      }, {
+        id: 'collect',
+        title: '收藏'
+      }, {
+        id: 'like',
+        title: '点赞'
+      }]
+    }
+  },
+
+  handleZanTabChange(e) {
+    let selectedId = e.selectedId;
+    this.setData({
+      'tab.selectedId': selectedId
+    })
+  }
 }));
